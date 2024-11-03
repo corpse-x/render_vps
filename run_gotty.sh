b5ccb0ca47da075603d4e59a93825eb96fee3673
ghost <error.2418362@gmail.com>
2024-11-02 20:45:26 -0400

    Update run_gotty.sh

--- run_gotty.sh
+++ run_gotty.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Define username and password (you can adjust these values)
+USERNAME="ghost"
+PASSWORD="linux"
 
-/usr/local/bin/gotty --permit-write --reconnect /bin/bash
+/usr/local/bin/gotty --permit-write --reconnect --credential "$USERNAME:$PASSWORD" /bin/bash
